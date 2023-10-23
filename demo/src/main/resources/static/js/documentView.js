f1a1416e52015b86bb9f16ed2dcd82e28fab3344
ppark__jjong <101304928+Parkjonghyeok2@users.noreply.github.com>
2023-10-23 16:04:02 +0900

    중간 커밋 템플릿 프로젝트 메인 진행율 디자인 수정

--- demo/src/main/resources/static/js/documentView.js
+++ demo/src/main/resources/static/js/documentView.js
@@ -1,5 +1,22 @@
 var floating = document.getElementById('floating1');
 
-floating.addEventListener('click', function(){
+floating.addEventListener('click', function () {
   this.classList.toggle('is-activate');
 });
+
+
+$.fn.ashCordian = function () {
+
+  var container = $(this);
+
+  container.find('header').click(function () {
+    if ($(this).siblings('section').css('display') == 'block') {
+      container.find('section').slideUp(150);
+    } else {
+      container.find('section').slideUp(150);
+      $(this).siblings('section').slideDown(150);
+    }
+  });
+};
+
+$('#accord1').ashCordian();
